98cfe1d782e703f7d2fe1226d2c3180a34a7300c
MaksymFilippov <MaksymFilippov@bitbucket.org>
2016-11-15 03:27:30 +0200

    Protractor_lesson2
    Done

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,4 +1,5 @@
 module.exports.config = {
+    
     useAllAngular2AppRoots: true,
     specs: 'spec.js',
     directConnect: true
--- spec.js
+++ spec.js
@@ -1,7 +1,31 @@
-describe('suite', function () {
-
-    it('test', function () {
-        browser.get('http://maxcabrera.com/code/todo-list/')
+let URL = 'http://maxcabrera.com/code/todo-list/'
+describe('Navigation TODO', function() {
+    it('should have correct title', function() {
+        browser.get(URL)
         expect(browser.getTitle()).toBe('Angular2 Seed')
+    })
+    it('should create a new task', function() {
+        browser.get(URL)
+        let webelement = $('input.enter-todo')
+        webelement.sendKeys('SL')
+        browser.driver.sleep(3000)
+        webelement.submit()
+        let task = $$('todo-list .small-12')
+        expect(task.getText()).toContain('SL')
+    })
+    it('should cancel a task', function() {
+        browser.get(URL)
+        let current = element.all(by.css('todo-list .small-12'))
+        element(by.css("input[type=checkbox]")).click();
+        let cancel = element.all(by.css('todo-list .small-12'))
+        browser.driver.sleep(3000)
+        expect(current.count() == cancel.count()).toBe(false)
+        it('should cancel all task', function() {
+            browser.get(URL);
+            expect(browser.getTitle()).toBe('Angular2 Seed');
+            let allcancel = element.all(by.css('.todo-container [type="checkbox"]'));
+            expect(allcancel.count()).toBe(0)
+
+        })
     })
 })
